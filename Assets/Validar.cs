06e20e029a4399f49a6e21aaebbb17e4f86271d7
facuzylberberg <101569051+Maiaguiter@users.noreply.github.com>
2022-04-11 23:41:53 -0300

    Ej04,05,06 finished

---
+++ Assets/Validar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Validar : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+    public GameObject cartel;
+    public Text textomsj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+        cartel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void validarContrasenia()
+    {
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            cartel.SetActive(true);
+            textomsj.text = "¡Bienvenido!";
+            Debug.Log("¡Bienvenido!");
+        }
+        else
+        {
+            cartel.SetActive(true);
+            textomsj.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
